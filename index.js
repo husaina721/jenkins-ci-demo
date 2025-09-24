6f0e0ccc3ea1269593109a47371cd8132728af0c
husaina721 <hussainagadi04@email.com>
2025-09-25 00:15:53 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello from Jenkins pipeline!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Homepage route
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from Jenkins deployed app!</h1><p>Welcome to your website.</p>');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`App is running at http://localhost:${port}`);
+});
